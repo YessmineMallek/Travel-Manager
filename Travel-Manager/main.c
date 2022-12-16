0d206245f4ca0eed93de55d8056ed8b278f9ce27
Yessmine mallek <mallek.yessmin@gmail.com>
2022-12-16 22:33:30 +0100

    structure

--- Travel-Manager/main.c
+++ Travel-Manager/main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Date
+{
+       int jour;
+       int mois;
+       int annee;
+} Date;
+typedef struct Vol
+{
+    int code ;
+    Date dateDep;
+    Date dateArriv;
+    char aeroportDep[30];
+    char aeroportArr[30];
+    int numAvion;
+    int nbreChaisesRest;//Pour les chaises restantes
+}Vol;
 int main()
 {
-    printf("Hello world!\n");
-    printf("heeello ");
+    //ouvrir le ficher avec le mode ecriture/lecture en fin du fichier (a+)
+    FILE *ficVol=fopen("vols.txt","a+");
+    int lettre=0;
+    if (ficVol == NULL)//tester si le ficher est ouvert
+        exit (1);
+   while (lettre != EOF)//EOF=End Of File
+   {
+        lettre =fgetc(ficVol);
+        printf("%c",lettre);
+   }
+
+
+
+
+
+
+
+    //Fermer le fichier
+    fclose(ficVol);
     return 0;
 }
